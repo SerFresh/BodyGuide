f5864338b97c6fde5fd9383ef5b04a0aed51a3fe
SerFresh <151444848+SerFresh@users.noreply.github.com>
2024-11-08 23:49:40 +0700

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('upload/', views.index),
-    path('',views.upload_basic),
+    path('guide/', views.index),
+    path('', views.upload_basic),
+    path('details/', views.detail),
+    path('members/', views.members)
 ]
 
 urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
