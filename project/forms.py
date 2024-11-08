49823f91505367574916e603385efe4ae48cb547
SerFresh <151444848+SerFresh@users.noreply.github.com>
2024-11-08 23:49:20 +0700

    Update forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 
 class UploadFrom(forms.Form):
-    file = forms.ImageField(required=True)
+    file = forms.ImageField(required=False)  # ไม่บังคับให้กรอกไฟล์ใหม่ทุกครั้ง
+    shape_choices = [
+        ('', '-- เลือกรูปร่างของคุณ --'),
+        ('Pear', 'รูปร่างลูกแพร์'),
+        ('Apple', 'รูปร่างแอปเปิ้ล'),
+        ('Inverted triangle', 'รูปร่างสามเหลี่ยมกลับหัว'),
+    ]
+    shape = forms.ChoiceField(choices=shape_choices, required=True, label="เลือกประเภทของรูปร่าง")
